b920ffbfd050067388ae443cf8abd78c3d8971d7
Maryna Kapula <maryna.kapula@gmail.com>
2026-01-26 13:48:05 +0200

    6.13  on

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
-import Filter from './components/Filter'  
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import Filter from './components/Filter'
 
-const App = () => {
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      <Filter />                          
-      <AnecdoteList />
-      <AnecdoteForm />
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <h2>Anecdotes</h2>
+    <Notification />      {/* ← вот здесь появится сообщение */}
+    <Filter />
+    <AnecdoteList />
+    <AnecdoteForm />
+  </div>
+)
 
 export default App
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,12 +1,23 @@
+// src/components/Notification.jsx
+import { useSelector } from 'react-redux'
+
 const Notification = () => {
+  const message = useSelector(state => state.notification)
+
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
-    marginBottom: 10
+    marginBottom: 10,
   }
 
-  return <div style={style}>render here notification...</div>
+  if (!message) return null
+
+  return (
+    <div style={style}>
+      {message}
+    </div>
+  )
 }
 
 export default Notification
