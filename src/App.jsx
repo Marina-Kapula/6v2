10569b0b9eb3d53e048406b75362d3f6e2dbb528
Maryna Kapula <maryna.kapula@gmail.com>
2026-01-27 13:57:40 +0200

    6/14 on

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import AnecdoteList from './components/AnecdoteList'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
+import { initializeAnecdotes } from './reducers/anecdoteSlice'
 
-const App = () => (
-  <div>
-    <h2>Anecdotes</h2>
-    <Notification />      {/* ← вот здесь появится сообщение */}
-    <Filter />
-    <AnecdoteList />
-    <AnecdoteForm />
-  </div>
-)
+
+const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeAnecdotes())
+  }, [dispatch])
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Notification />
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
 
 export default App
